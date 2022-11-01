5bdfd718508016b3fb6979a6ef56749bfda3f81b
JongukYang <didwhddnr123@naver.com>
2022-11-01 11:51:01 +0900

    new upload

---
+++ Java-CT/src/Level_1/Pro_CT_14.java
@@ -0,0 +1,14 @@
+package Level_1;
+
+public class Pro_CT_14 {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_14 sol = new Pro_CT_14();
+        System.out.println(sol.solution(3, 5));
+    }
+}
